ca037d98bb8d423d7d9e3fa84e4224c25be5fade
Ben-Hur <benhurbueno.adv@gmail.com>
2022-12-19 16:15:01 -0300

    finished cep

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -16,7 +16,7 @@ import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { api } from "../../services/api.js";
 
@@ -31,56 +31,78 @@ export function SignUp() {
   const [streetNumber, setStreetNumber] = useState("");
   const [neighborhood, setNeighborhood] = useState("");
   const [city, setCity] = useState("");
-  const [state, setState] = useState("");
+  const [uf, setUF] = useState("");
   const [complement, setComplement] = useState("");
 
   function handleSignUp() {
-    if (!name || !email || !password) {
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !register ||
+      !phone ||
+      !street ||
+      !streetNumber ||
+      !neighborhood ||
+      !city ||
+      !uf
+    ) {
       return alert("Preencha todos os campos!");
     }
 
-    fetch(`${api.baseURL}/users`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ name, email, password })
-    })
-      .then(response => {
-        if (response.ok) {
-          return alert("Usuário criado com sucesso!");
-        }
-        return response.text().then(text => {
-          throw new Error(text);
-        });
+    const regexCPF = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/;
+
+    if (register.match(regexCPF)) {
+      fetch(`${api.baseURL}/users`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name, email, password })
       })
-      .catch(e => {
-        if (e) {
-          const errorObject = JSON.parse(e.message);
-          alert(errorObject.message);
-        } else {
-          alert("Um erro ocorreu!");
-        }
-      });
+        .then(response => {
+          if (response.ok) {
+            return alert("Usuário criado com sucesso!");
+          }
+          return response.text().then(text => {
+            throw new Error(text);
+          });
+        })
+        .catch(e => {
+          if (e) {
+            const errorObject = JSON.parse(e.message);
+            alert(errorObject.message);
+          } else {
+            alert("Um erro ocorreu!");
+          }
+        });
+    } else {
+      return alert("CPF Inválido!");
+    }
   }
 
-  const checkCEP = e => {
-    // Tudo que for de 0 a 9 ele vai encontrar e substituir por vazio.
-    const cep = e.target.value.replace(/\D/g, "");
+  useEffect(() => {
+    if (postalcode.length === 8) {
+      async function getCEP() {
+        const cep = await fetch(`https://viacep.com.br/ws/${postalcode}/json/`)
+          .then(res => res.json())
+          .then(data => data);
 
-    // Se existir o value ou se for diferente de 8 a função irá retornar.
-    if (cep?.length !== 8) {
-      return;
+        setCity(cep.localidade);
+        setNeighborhood(cep.bairro);
+        setStreet(cep.logradouro);
+        setUF(cep.uf);
+      }
+      getCEP();
     }
+  }, [postalcode]);
 
-    // Para capturar os dados através de um request, transformar em json e jogar no console o endereço referente ao cep.
-    fetch(`https://viacep.com.br/ws/${cep}/json/`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-      });
-  };
+  function checkCEP() {
+    if (postalcode.length !== 8) {
+      return alert("CEP Incompleto!");
+    }
+  }
 
   return (
     <Container>
@@ -91,74 +113,85 @@ export function SignUp() {
         <h2>Crie sua conta</h2>
         <Input
           icon={FiUser}
+          value={name}
           placeholder="Nome"
           type="text"
           onChange={e => setName(e.target.value)}
         />
         <Input
           icon={FiMail}
+          value={email}
           placeholder="E-mail"
           type="text"
           onChange={e => setEmail(e.target.value)}
         />
         <Input
           icon={FiLock}
+          value={password}
           placeholder="Senha"
           type="password"
           onChange={e => setPassword(e.target.value)}
         />
         <Input
           icon={HiOutlineIdentification}
+          value={register}
           placeholder="CPF"
-          type="number"
+          type="text"
           onChange={e => setRegister(e.target.value)}
         />
         <Input
           icon={FiPhone}
           placeholder="Phone"
+          value={phone}
           type="phone"
           onChange={e => setPhone(e.target.value)}
         />
         <Input
           icon={FiCodepen}
           placeholder="CEP"
-          type="text"
-          onBlur={e => {
-            setPostalCode(e.target.value);
-          }}
+          value={postalcode}
+          type="number"
+          onChange={e => setPostalCode(e.target.value)}
+          onBlur={checkCEP}
         />
         <Input
           icon={FiMapPin}
+          value={street}
           placeholder="Rua"
           type="text"
           onChange={e => setStreet(e.target.value)}
         />
         <Input
           icon={AiOutlineFieldNumber}
+          value={streetNumber}
           placeholder="Número"
           type="text"
           onChange={e => setStreetNumber(e.target.value)}
         />
         <Input
           icon={FiMapPin}
+          value={neighborhood}
           placeholder="Bairro"
           type="text"
           onChange={e => setNeighborhood(e.target.value)}
         />
         <Input
           icon={FiMapPin}
+          value={city}
           placeholder="Cidade"
           type="text"
           onChange={e => setCity(e.target.value)}
         />
         <Input
           icon={FiMapPin}
+          value={uf}
           placeholder="Estado"
           type="text"
-          onChange={e => setState(e.target.value)}
+          onChange={e => setUF(e.target.value)}
         />
         <Input
           icon={FiTag}
+          value={complement}
           placeholder="Complemento"
           type="text"
           onChange={e => setComplement(e.target.value)}
